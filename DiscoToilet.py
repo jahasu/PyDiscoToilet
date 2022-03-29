6c7373af214457b2c42b24414914eec924388b51
Jack <Jacksutcliffe@outlook.com>
2022-03-29 17:20:31 +1300

    a

--- DiscoToilet.py
+++ DiscoToilet.py
@@ -1,31 +1,47 @@
 from DmxPy import DmxPy
 import RPi.GPIO as GPIO
 import time
+import pygame
 
 button_pin = 26
 light_r= 23
 dmx_r = 24
 
-GPIO.setmode(GPIO.BOARD)
+running = 0
+
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(light_r, GPIO.OUT)
 GPIO.setup(dmx_r, GPIO.OUT)
-GPIO.setup(button_pin,GPIO.IN)
+GPIO.setup(button_pin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
 dmx = DmxPy('/dev/ttyUSB0')
+pygame.mixer.init()
 
-def rest_lights():
+def reset_lights():
     dmx.blackout()
     dmx.render()
 
-# def turn_on_lights(scene):
-#     #turn on lights based on numebr of taps 
-#     if scene == 1:
+def set_scene(scene):
+  #turn on lights based on numebr of taps 
+    if scene == 1:
+        print("scene 1")
+        pygame.mixer.music.play
+        
+        
 
-#     if scene == 2:
+
+    if scene == 2:
+        print("scene 2")
+
+
 
 while True:
-  
-    print(GPIO.input(button_pin))
+    if running == 0 and GPIO.input(button_pin) == 1:
+        pygame.mixer.music.load('/home/pi/Desktop/PyDiscoToilet/music/1.mp3')
+        set_scene(1)
+        #button pressed 
+
+
 
 
 print('Stopping')
