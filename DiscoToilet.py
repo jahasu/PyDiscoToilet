49e4577d28adc3e94bbc0073f819daa6ccda1e5d
Jack <Jacksutcliffe@outlook.com>
2022-03-29 16:31:47 +1300

    hello

--- DiscoToilet.py
+++ DiscoToilet.py
@@ -2,15 +2,31 @@ from DmxPy import DmxPy
 import RPi.GPIO as GPIO
 import time
 
+button_pin = 3
+light_r= 23
+dmx_r = 24
+
+GPIO.setup(light_r, GPIO.OUT)
+GPIO.setup(dmx_r, GPIO.OUT)
+GPIO.setup(button_pin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+dmx = DmxPy('/dev/ttyUSB0')
+
 def rest_lights():
     dmx.blackout()
     dmx.render()
 
-def: turn_on_lights(n_taps):
+def turn_on_lights(scene):
     //turn on lights based on numebr of taps 
+    if scene == 1:
+    if scene == 2:
 
-def: main()
+while True:
+    if GPIO.input(button_pin) == 0 :
+        print("Pin High")
+        turn_on_lights(1)
+    else:
+        print("looping")
 
 
-while true:
-    
+print('Stopping')
