c36ba4795667249fcbb1be52c0677f488f85d4c4
Jack <Jacksutcliffe@outlook.com>
2022-03-29 15:56:40 +1300

    commit

---
+++ DiscoToilet.py
@@ -0,0 +1,12 @@
+from DmxPy import DmxPy
+import RPi.GPIO as GPIO
+import time
+
+def rest_lights():
+    dmx.blackout()
+    dmx.render()
+
+def: turn_on_lights(n_taps):
+    //turn on lights based on numebr of taps 
+
+def: 
