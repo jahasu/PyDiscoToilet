ad435882fd7f729ec21fd3c1f73abd593641850f
Jack <Jacksutcliffe@outlook.com>
2022-03-29 17:58:06 +1300

    CoMmIt

--- DiscoToilet.py
+++ DiscoToilet.py
@@ -4,42 +4,85 @@ import time
 import pygame
 
 button_pin = 26
-light_r= 23
-dmx_r = 24
+relay_1= 23
+relay_2 = 24
 
 running = 0
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(light_r, GPIO.OUT)
-GPIO.setup(dmx_r, GPIO.OUT)
+GPIO.setup(relay_1), GPIO.OUT)
+GPIO.setup(relay_2, GPIO.OUT)
 GPIO.setup(button_pin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
 dmx = DmxPy('/dev/ttyUSB0')
 pygame.mixer.init()
 
-def reset_lights():
+def restore():
     dmx.blackout()
     dmx.render()
+    pygame.mixer.music.stop()
+    pygame.mixer.music.unload()
+
+
+def set_dmx(scene):
+    if scene == 1:
+        print("enable Scene1 dmx")
+        dmx.setChannel(1,255)
+        dmx.setChannel(2, 255)
+        dmx.setChannel(3, 255)
+        dmx.setChannel(4, 0)
+        dmx.render()
+        
+    if scene == 2: 
+        print("enable Scene2 dmx")
+
+def set_relays(state): #1 turns main lights on 
+    if state:
+        GPIO.output(relay_1, GPIO.LOW)
+        GPIO.output(relay_2, GPIO.HIGH)
+    else:
+        GPIO.output(relay_1, GPIO.HIGH)
+        GPIO.output(relay_2, GPIO.LOW)
 
 def set_scene(scene):
-  #turn on lights based on numebr of taps 
+    #kill lights 
+    #delay an ammount 
+    
+
+    #turn on lights based on numebr of taps 
+    set_lights(scene)
+
     if scene == 1:
         print("scene 1")
-        pygame.mixer.music.play
+        pygame.mixer.music.load('/home/pi/Desktop/PyDiscoToilet/music/1.mp3')
+        pygame.mixer.music.play(1)
         
         
 
 
     if scene == 2:
         print("scene 2")
-
-
-
-while True:
-    if running == 0 and GPIO.input(button_pin) == 1:
         pygame.mixer.music.load('/home/pi/Desktop/PyDiscoToilet/music/1.mp3')
-        set_scene(1)
-        #button pressed 
+        pygame.mixer.music.play(1)
+
+
+## while statement runs the code 
+while True: 
+
+    if GPIO.input(button_pin) == 0: 
+
+        if running: 
+            running = False
+            restore()
+            set_relays(1)
+
+        else:
+            restore()
+            set_relays(0)
+            running = True
+
+            set_scene(2)
+            #button pressed 
 
 
 
